d37f614707e8fa6878ce4ea9534ba301a862c140
Naufal Adi Wijanarko <naufaladi35@gmail.com>
2024-03-02 15:37:19 +0700

    fix: home page layout

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { Oswald } from "next/font/google";
 import Link from "next/link";
 
@@ -5,8 +8,9 @@ export const oswald = Oswald({ subsets: ["latin"] });
 
 export const Navbar = () => {
   return (
-    <nav className="flex flex-auto px-12 py-6">
-      <p className={oswald.className + " text-4xl"}>tdashai</p>
+    <nav className="flex flex-auto px-12 py-6 justify-center content-center gap-4">
+      <Icon icon="ooui:vision-simulator" width="60px" height="60px" style={{border: "1px solid white", flexShrink: 0}}/>
+      <p className={oswald.className} style={{fontSize: "24px"}}>tdashai</p>
       <div className="flex flex-1 justify-center content-center">
         <NavButton>Home</NavButton>
         <NavButton>Our Projects</NavButton>
@@ -17,7 +21,7 @@ export const Navbar = () => {
 
 const NavButton = ({ children, href }: { children : React.ReactNode, href? : string }) => {
   return (
-    <button className="p-4 text-lg">
+    <button className="p-4 text-md">
       <Link href={href ?? "/"}>{children}</Link>
     </button>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-fit flex-row flex-wrap-reverse items-center justify-center w-full m-auto p-3 max-w-[1200px]">
-      <div className="mt-2 lg:w-1/2">
+    <div className="flex h-fit flex-row flex-wrap-reverse items-center justify-center w-full m-auto px-6 py-12 max-w-[1200px]">
+      <div className="mt-2 lg:w-2/3">
         <h1 className="text-[#E08AED] font-bold text-6xl">Exploring & Developing Possibilities of <span style={{'color': "#9DB3E2"}}>AI</span></h1>
         <p className="text-2xl my-6">We are a group of developers that are interested in developing products utilizing the continuously growing potential of <span style={{'color': "#9DB3E2"}}>artificial intelligence.</span></p>
         <button 
@@ -20,13 +20,13 @@ export default function Home() {
           Our Projects
         </button>
       </div>
-      <div className="lg:w-1/2">
+      <div className="w-full lg:w-1/3">
         <Image src={`/image/homepage_ai.png`} 
           alt="" 
           width="0"
           height="0"
           sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
+          className="m-auto w-1/2 lg:w-full"
           />
       </div>
 

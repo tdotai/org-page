da22743996dd6dfcee6a830b312fd56666faed0d
Naufal Adi Wijanarko <naufaladi35@gmail.com>
2024-03-02 16:15:38 +0700

    refactor: layout & button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex h-fit flex-row flex-wrap-reverse items-center justify-center w-full m-auto px-6 py-12 max-w-[1200px]">
+    <div className="flex h-fit flex-row flex-wrap-reverse items-center justify-center">
       <div className="mt-2 lg:w-2/3">
         <h1 className="text-[#E08AED] font-bold text-6xl">Exploring & Developing Possibilities of <span style={{'color': "#9DB3E2"}}>AI</span></h1>
         <p className="text-2xl my-6">We are a group of developers that are interested in developing products utilizing the continuously growing potential of <span style={{'color': "#9DB3E2"}}>artificial intelligence.</span></p>
         <Link href="/projects">
-          <button 
-            style={{
-              backgroundColor : "#9DB3E2", 
-              width:"300px", 
-              height:"64px", 
-              fontWeight:"bold", 
-              fontSize:"28px",
-              boxShadow: "10px 10px #E08AED",
-              }}
-            >
-            Our Projects
-          </button>
+          <button type="button" className="custom">Our Projects</button>
         </Link>
 
       </div>
